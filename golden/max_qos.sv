901a8b326089e152535406bcaf70752b3aa93882
koteswar059 <koteswar059@gmail.com>
2026-02-24 06:36:22 +0000

    main branch updated to have single module as well

--- golden/max_qos.sv
+++ golden/max_qos.sv
@@ -1,4 +1,71 @@
 `timescale 1us/1us
+
+module bin2bar_tree (
+ i_bin,
+ o_bar
+);
+ 
+  parameter   BIN_WIDTH = 2;
+  localparam  BAR_WIDTH = 2**BIN_WIDTH;
+  
+  localparam  BAR_WIDTH_L = BAR_WIDTH/2;
+  
+  input  [BIN_WIDTH-1:0] i_bin;
+  output [BAR_WIDTH-1:0] o_bar;
+  
+  wire   [BAR_WIDTH_L -1 : 0] bar_temp;
+  
+  generate
+    if (BIN_WIDTH == 1) begin
+      assign o_bar = {i_bin,1'b1};
+    end
+    else
+      begin : gen_bin2bar_tree_instance
+        bin2bar_tree #(
+          .BIN_WIDTH(BIN_WIDTH-1)
+        ) u_bin2bar_tree
+        (
+          .i_bin(i_bin[BIN_WIDTH-2:0]),
+          .o_bar(bar_temp)
+        );
+        
+        assign o_bar = {
+          { {BAR_WIDTH_L{i_bin[BIN_WIDTH-1]}} & bar_temp } ,
+          { {BAR_WIDTH_L{i_bin[BIN_WIDTH-1]}} | bar_temp }
+        };
+      end
+  endgenerate
+
+endmodule
+
+module bin2bar(
+  input [2:0] i_bin,
+  output [7:0] o_bar
+);
+  
+  bin2bar_tree #( .BIN_WIDTH(3))
+  u_bin2bar_tree (
+    .i_bin(i_bin),
+    .o_bar(o_bar)
+  );
+  
+endmodule
+
+module bar2bin (
+  input  [7:0] i_bar,
+  output [2:0] o_bin
+);
+  
+  assign o_bin[0] = (i_bar[1] & ~i_bar[2]) | (i_bar[3] & ~i_bar[4]) | (i_bar[5] & ~i_bar[6] ) | i_bar[7];
+  
+  assign o_bin[1] = (i_bar[2] & ~i_bar[4]) | i_bar[6];
+  
+  assign o_bin[2] = i_bar[4];
+  
+endmodule
+
+
+
 module max_qos(
   
   input clk,
